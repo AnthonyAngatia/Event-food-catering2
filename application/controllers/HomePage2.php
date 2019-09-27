0d939623009efcabd7059948ffafa7510235d86b
Omi1 <anthonyangatia@gmail.com>
2019-09-27 09:34:12 +0300

    After user has logged in page

---
+++ application/controllers/HomePage2.php
@@ -0,0 +1,16 @@
+<?php
+defined('BASEPATH') or exit('No direct script allowed');
+class HomePage extends CI_Controller
+{
+
+    public function index()
+    {
+        $this->load->view('HomePage2');
+        /*----Loading multiple files..
+
+        //$this->load->view('');
+        //$this->load->view('');	
+        //$this->load->view('');
+        */
+    }
+}
