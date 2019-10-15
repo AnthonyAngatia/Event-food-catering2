77b4e02a2a433fd8da730cd478fd7732717395b0
Omi1 <anthonyangatia@gmail.com>
2019-10-15 17:40:11 +0300

    Update on the login button and session variable display on the nav bar + footer is added

--- application/views/users/account.php
+++ application/views/users/account.php
@@ -10,202 +10,343 @@ defined('BASEPATH') or exit('No direct script access allowed');
     <meta http-equiv="X-UA-Compatible" content="ie=edge" />
     <title>Taste of Africa</title>
     <style>
-        * {
-            box-sizing: border-box;
-            margin: 0;
-            padding: 0px;
+    * {
+        box-sizing: border-box;
+        margin: 0;
+        padding: 0px;
+    }
+
+    h1 {
+        text-align: center;
+    }
+
+    header {
+        display: flex;
+        justify-content: flex-end;
+    }
+
+    .logo {
+        height: 60px;
+        width: 60px;
+        border: 1px solid tomato;
+        margin-right: auto;
+        padding: 5px;
+    }
+
+    .header h1 {
+        font-family: cursive;
+        margin-right: auto;
+        padding: 5px;
+    }
+
+    .Sign-up button {
+        border: 1px solid #002147;
+        color: #002147;
+        padding: 7px;
+        border-radius: 50px;
+        cursor: pointer;
+        background-color: white;
+    }
+
+    a {
+        padding: 15px;
+    }
+
+    .nav {
+        display: flex;
+        justify-content: space-evenly;
+        background-color: #002147;
+    }
+
+    button {
+        border: 1px solid aliceblue;
+        border-radius: 50px;
+        background-color: #002147;
+        padding: 7px;
+        color: white;
+        cursor: pointer;
+    }
+
+    button:hover {
+        color: powderblue;
+    }
+
+    .mySlides {
+        display: none;
+    }
+
+    /* Slideshow container */
+    .slideshow-container {
+        max-width: 100%;
+        position: relative;
+        margin: auto;
+        max-height: 900px;
+        background-color: #ddd;
+    }
+
+    .mySlides img {
+        max-height: 610px;
+    }
+
+    /* Caption text */
+    .text {
+        color: #f2f2f2;
+        font-size: 15px;
+        padding: 8px 12px;
+        position: absolute;
+        bottom: 8px;
+        width: 100%;
+        text-align: center;
+    }
+
+    .text button {
+        font-size: 24px;
+        border-radius: 0px;
+        width: 150px;
+    }
+
+    /* Number text (1/3 etc) */
+    .numbertext {
+        color: #f2f2f2;
+        font-size: 12px;
+        padding: 8px 12px;
+        position: absolute;
+        top: 0;
+    }
+
+    /* The dots/bullets/indicators */
+    .dot {
+        height: 15px;
+        width: 15px;
+        margin: 0 2px;
+        background-color: #bbb;
+        border-radius: 50%;
+        display: inline-block;
+        transition: background-color 0.6s ease;
+    }
+
+    .active {
+        background-color: #717171;
+    }
+
+    /* Fading animation */
+    .fade {
+        -webkit-animation-name: fade;
+        -webkit-animation-duration: 0.5s;
+        animation-name: slide;
+        animation-duration: 0.5s;
+    }
+
+    @-webkit-keyframes fade {
+        from {
+            opacity: 0.4;
         }
-        h1 {
-            text-align: center;
+
+        to {
+            opacity: 1;
         }
-        header {
-            display: flex;
-            justify-content: flex-end;
+    }
+
+    @keyframes fade {
+        from {
+            opacity: 0.4;
         }
-        .logo {
-            height: 60px;
-            width: 60px;
-            border: 1px solid tomato;
-            margin-right: auto;
-            padding: 5px;
+
+        to {
+            opacity: 1;
         }
-        .header h1 {
-            font-family: cursive;
-            margin-right: auto;
-            padding: 5px;
-        }
-        .Sign-up button {
-            border: 1px solid #003366;
-            color: #003366;
-            padding: 7px;
-            border-radius: 50px;
-            cursor: pointer;
-            background-color: white;
-        }
-        a {
-            padding: 15px;
-        }
-        .nav {
-            display: flex;
-            justify-content: space-evenly;
-            background-color: #003366;
-        }
-        button {
-            border: 1px solid aliceblue;
-            border-radius: 50px;
-            background-color: #003366;
-            padding: 7px;
-            color: white;
-            cursor: pointer;
-        }
-        button:hover {
-            color: powderblue;
-        }
-        .mySlides {
-            display: none;
-        }
-        /* Slideshow container */
-        .slideshow-container {
-            max-width: 100%;
-            position: relative;
-            margin: auto;
-            max-height: 900px;
-            background-color: #ddd;
-        }
-        .mySlides img {
-            max-height: 610px;
-        }
-        /* Caption text */
+    }
+
+    /* On smaller screens, decrease text size */
+    @media only screen and (max-width: 300px) {
         .text {
-            color: #f2f2f2;
-            font-size: 15px;
-            padding: 8px 12px;
-            position: absolute;
-            bottom: 8px;
-            width: 100%;
-            text-align: center;
+            font-size: 11px;
         }
-        .text button {
-            font-size: 24px;
-            border-radius: 0px;
-            width: 150px;
-        }
-        /* Number text (1/3 etc) */
-        .numbertext {
-            color: #f2f2f2;
-            font-size: 12px;
-            padding: 8px 12px;
-            position: absolute;
-            top: 0;
-        }
-        /* The dots/bullets/indicators */
-        .dot {
-            height: 15px;
-            width: 15px;
-            margin: 0 2px;
-            background-color: #bbb;
-            border-radius: 50%;
-            display: inline-block;
-            transition: background-color 0.6s ease;
-        }
-        .active {
-            background-color: #717171;
-        }
-        /* Fading animation */
-        .fade {
-            -webkit-animation-name: fade;
-            -webkit-animation-duration: 0.5s;
-            animation-name: slide;
-            animation-duration: 0.5s;
-        }
-        @-webkit-keyframes fade {
-            from {
-                opacity: 0.4;
-            }
-            to {
-                opacity: 1;
-            }
-        }
-        @keyframes fade {
-            from {
-                opacity: 0.4;
-            }
-            to {
-                opacity: 1;
-            }
-        }
-        /* On smaller screens, decrease text size */
-        @media only screen and (max-width: 300px) {
-            .text {
-                font-size: 11px;
-            }
-        }
-        /*  @media (min-width: 768px) */
-        .Dishes {
-            display: grid;
-            grid-template-columns: 1fr 1fr 1fr;
-            grid-gap: 1em;
-            grid-auto-rows: minmax(300px, auto);
-            margin-left: 5px;
-            margin-right: 5px;
-        }
-        .Dishes img {
-            max-height: 300px;
-            height: 100%;
-            width: 100%;
-        }
-        .Extras {
-            display: grid;
-            grid-template-columns: 1fr 1fr 1fr;
-            grid-gap: 1em;
-            grid-auto-rows: minmax(300px, auto);
-            margin-left: 5px;
-            margin-right: 5px;
-        }
-        .Extras img {
-            max-height: 300px;
-            height: 100%;
-            width: 100%;
-        }
-        .Extras div {
-            background-color: #ddd;
-        }
-        .Description {
-            display: grid;
-            grid-template-columns: 1fr 1fr 1fr;
-            grid-gap: 1em;
-            grid-auto-rows: minmax(100px, auto);
-            margin-left: 5px;
-            margin-right: 5px;
-        }
-        .Description 2 {
-            display: grid;
-            grid-template-columns: 1fr 1fr 1fr;
-            grid-gap: 1em;
-            grid-auto-rows: minmax(100px, auto);
-            margin-left: 5px;
-            margin-right: 5px;
-        }
-        .Dishes div {
-            background-color: #ddd;
-        }
-        .Description div {
-            background-color: #dddd;
-            box-shadow: 0 8px 6px -6px black;
-            padding: 2em;
-        }
-        .Description2 div {
-            background-color: #dddd;
-            box-shadow: 0 8px 6px -6px black;
-            padding: 2em;
-        }
-        footer {
-            margin-top: 1em;
-            background-color: #003366;
-            color: white;
-            text-align: center;
-            font-size: 24px;
-        }
+    }
+
+    /*  @media (min-width: 768px) */
+    .Dishes {
+        display: grid;
+        grid-template-columns: 1fr 1fr 1fr;
+        grid-gap: 1em;
+        grid-auto-rows: minmax(300px, auto);
+        margin-left: 5px;
+        margin-right: 5px;
+    }
+
+    .Dishes img {
+        max-height: 300px;
+        height: 100%;
+        width: 100%;
+    }
+
+    .Extras {
+        display: grid;
+        grid-template-columns: 1fr 1fr 1fr;
+        grid-gap: 1em;
+        grid-auto-rows: minmax(300px, auto);
+        margin-left: 5px;
+        margin-right: 5px;
+    }
+
+    .Extras img {
+        max-height: 300px;
+        height: 100%;
+        width: 100%;
+    }
+
+    .Extras div {
+        background-color: #ddd;
+    }
+
+    .Description {
+        display: grid;
+        grid-template-columns: 1fr 1fr 1fr;
+        grid-gap: 1em;
+        grid-auto-rows: minmax(100px, auto);
+        margin-left: 5px;
+        margin-right: 5px;
+    }
+
+    .Description 2 {
+        display: grid;
+        grid-template-columns: 1fr 1fr 1fr;
+        grid-gap: 1em;
+        grid-auto-rows: minmax(100px, auto);
+        margin-left: 5px;
+        margin-right: 5px;
+    }
+
+    .Dishes div {
+        background-color: #ddd;
+    }
+
+    .Description div {
+        background-color: #dddd;
+        box-shadow: 0 8px 6px -6px black;
+        padding: 2em;
+    }
+
+    .Description2 div {
+        background-color: #dddd;
+        box-shadow: 0 8px 6px -6px black;
+        padding: 2em;
+    }
+
+    .full {
+        width: 100%;
+    }
+
+    .gap {
+        height: 30px;
+        width: 100%;
+        clear: both;
+        display: block;
+    }
+
+    .footer {
+        background: #002147;
+        height: auto;
+        padding-bottom: 30px;
+        position: relative;
+        width: 100%;
+        border-bottom: 1px solid #CCCCCC;
+        border-top: 1px solid #DDDDDD;
+    }
+
+    .footer p {
+        margin: 0;
+    }
+
+    .footer img {
+        max-width: 100%;
+    }
+
+    .footer h3 {
+        color: white;
+        font-size: 18px;
+        font-weight: 600;
+        line-height: 27px;
+        padding: 40px 0 0px;
+        text-transform: uppercase;
+        margin-bottom: 15px;
+    }
+
+    .footer h4 {
+        color: white;
+        font-size: 2em;
+        font-weight: 600;
+        line-height: 38px;
+        padding: 40px 0 10px;
+        font-family: cursive;
+        font-weight: lighter
+    }
+
+    .footer ul {
+        font-size: 13px;
+        list-style-type: none;
+        margin-left: 0;
+        padding-left: 0;
+        margin-top: 0px;
+        color: #7F8C8D;
+        padding: 0 0 8px 0;
+    }
+
+    .email {
+        border-bottom: 3px solid #fff;
+    }
+
+    .footer ul li a {
+        padding: 0 0 12px 0;
+        display: block;
+    }
+
+    .footer a {
+        color: white;
+        font-weight: lighter;
+    }
+
+    .footer p {
+        color: white;
+        font-weight: lighter;
+        font-size:
+    }
+
+    .footer a:hover {
+        text-decoration: none;
+        font-weight: bold;
+    }
+
+    .supportLi h4 {
+        font-size: 20px;
+        font-weight: lighter;
+        line-height: normal;
+        margin-bottom: 0 !important;
+        padding-bottom: 0;
+    }
+
+    .bg-gray {
+        background-image: -moz-linear-gradient(center bottom, #BBBBBB 0%, #F0F0F0 100%);
+        box-shadow: 0 1px 0 #B4B3B3;
+    }
+
+    }
+
+    .footer a {
+        color: #78828D
+    }
+
+    .footer-bottom {
+        margin-top: 2em;
+        border-top: 1px solid #DDDDDD;
+        padding-top: 20px;
+        padding-bottom: 10px;
+    }
+
+    .footer-bottom p.pull-left {
+        padding-top: 6px;
+        font-size: 0.75em
+    }
     </style>
 </head>
 
@@ -215,10 +356,27 @@ defined('BASEPATH') or exit('No direct script access allowed');
         <img class="logo" src="<?php echo base_url("Assets/logo.jpg"); ?>" />
         <h1>Taste of Africa</h1>
         <div id="sessionVariable">
-        <img class="logo" src="<?php echo base_url("Assets/PRO.png"); ?>" />
-        <p><b><?php echo $user['first_name'].' '.$user['last_name']; ?></b></p>
-        <a href="<?php echo base_url('index.php/Users/login'); ?>"> Log Out </a>
+            <div id="login-signup-button">
+                <a class="login" href="#" onclick="window.location.replace('users/login');"><button>Login</button></a>
+                <a class="Sign-up" href="#" onclick="window.location.replace('users/registration');"><button>Sign
+                        up</button></a>
+            </div>
+            <div id="session-data">
+                <img class="logo" src="<?php echo base_url("Assets/PRO.png"); ?>" />
+                <p><b><?php echo $user['first_name'].' '.$user['last_name']; ?></b></p>
+                <a href="<?php echo base_url('index.php/Users/login'); ?>"> Log Out </a>
+            </div>
         </div>
+        <script>
+        var session = "<?php echo $user['first_name'] ?>";
+        if (session == null) {
+            // alert("no session");
+            document.getElementById('session-data').style.display = "none";
+        } else {
+            // alert("found");
+            document.getElementById('login-signup-button').style.display = "none";
+        }
+        </script>
 
     </header>
     <div class="nav">
@@ -302,45 +460,102 @@ defined('BASEPATH') or exit('No direct script access allowed');
         <div class="desc1">
             <h1>Catering Services</h1>
 
-            <button style=" margin:1em; padding:1em;"><a href="" style="text-decoration:none; color:white ">Find out more</a></button>
+            <button style=" margin:1em; padding:1em;"><a href="" style="text-decoration:none; color:white ">Find out
+                    more</a></button>
         </div>
         <div class="desc2">
             <h1>Zero Waiting time</h1>
-            <button style=" margin:1em; padding:1em;"><a href="" style="text-decoration:none; color:white ">Find out more</a></button>
+            <button style=" margin:1em; padding:1em;"><a href="" style="text-decoration:none; color:white ">Find out
+                    more</a></button>
         </div>
         <div class="desc3">
             <h1>Quality Food</h1>
-            <button style=" margin:1em; padding:1em;"><a href="" style="text-decoration:none; color:white ">Find out more</a></button>
+            <button style=" margin:1em; padding:1em;"><a href="" style="text-decoration:none; color:white ">Find out
+                    more</a></button>
         </div>
     </div>
     <footer>
-        <p>CopyRight &copy; 2019</p>
+        <div class="footer" id="footer">
+            <div class="container">
+                <div class="row">
+                    <div class="col-lg-6 col-md-6 col-sm-6 col-xs-6">
+                        <h4> Taste of Africa </h4>
+                    </div>
+                    <div class="col-lg-3 col-sm-2 col-xs-3">
+                        <h3> Contact </h3>
+                        <ul>
+                            <li><a class="email" href="#"> TasteOfAfrica@gmail.com </a></li>
+                            <br />
+                            <li>
+                                <p>0700000000 </p>
+                            </li>
+                            <li>
+                                <p> 0700000000 </p>
+                            </li>
+                        </ul>
+                    </div>
+                    <div class="col-lg-3 col-sm-2 col-xs-3">
+                        <ul>
+                            <li>
+                                <h5> <a href="#" style="margin-top: 5em"> ABOUT US</a>
+                                    <h5>
+                            </li>
+                            <li>
+                                <h5><a href="#"> CURRENT SERIES </a>
+                                    <h5>
+                            </li>
+                            <li>
+                                <h5><a href="#"> THE HOUSE </a>
+                                    <h5>
+                            </li>
+                            <li>
+                                <h5><a href="#"> LOOKING BACK </a>
+                                    <h5>
+                            </li>
+                        </ul>
+                    </div>
+
+                    <!--/.row-->
+                </div>
+                <!--/.container-->
+            </div>
+            <!--/.footer-->
+
+            <div class="footer-bottom">
+                <div class="container">
+                    <p class="pull-left copyright"> Copyright © Footer 2019. All right reserved. </p>
+
+                </div>
+            </div>
+            <!--/.footer-bottom-->
+
     </footer>
 
     <script>
-        function myFunction() {
-            document.getElementByClassName("login").style.display = "block";
+    function myFunction() {
+        document.getElementByClassName("login").style.display = "block";
+    }
+    var slideIndex = 0;
+    showSlides();
+
+    function showSlides() {
+        var i;
+        var slides = document.getElementsByClassName("mySlides");
+        var dots = document.getElementsByClassName("dot");
+        for (i = 0; i < slides.length; i++) {
+            slides[i].style.display = "none";
         }
-        var slideIndex = 0;
-        showSlides();
-        function showSlides() {
-            var i;
-            var slides = document.getElementsByClassName("mySlides");
-            var dots = document.getElementsByClassName("dot");
-            for (i = 0; i < slides.length; i++) {
-                slides[i].style.display = "none";
-            }
-            slideIndex++;
-            if (slideIndex > slides.length) {
-                slideIndex = 1;
-            }
-            for (i = 0; i < dots.length; i++) {
-                dots[i].className = dots[i].className.replace(" active", "");
-            }
-            slides[slideIndex - 1].style.display = "block";
-            dots[slideIndex - 1].className += " active";
-            setTimeout(showSlides, 5000); // Change image every 2 seconds
+        slideIndex++;
+        if (slideIndex > slides.length) {
+            slideIndex = 1;
         }
+        for (i = 0; i < dots.length; i++) {
+            dots[i].className = dots[i].className.replace(" active", "");
+        }
+        slides[slideIndex - 1].style.display = "block";
+        dots[slideIndex - 1].className += " active";
+        setTimeout(showSlides, 5000); // Change image every 2 seconds
+    }
     </script>
 </body>
 
